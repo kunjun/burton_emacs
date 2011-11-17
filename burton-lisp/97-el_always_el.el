9aa6a9483e069bcea6dac8aef9940d388df4b058
unknown <burton@burton-PC.(none)>
2011-11-17 18:47:04 +0800

    auto complete | tabbar

--- burton-lisp/97-el_always_el.el
+++ burton-lisp/97-el_always_el.el
@@ -16,11 +16,44 @@
 (set-selection-coding-system 'gbk-unix)
 ;;;################### /粘贴剪贴板中文乱码解决 ###################
 
+;;;################### tabbar mode ###################
 ;;tabbar mode
 (require 'tabbar)
 (tabbar-mode 1)
-(global-set-key [(meta j)] 'tabbar-backward)
-(global-set-key [(meta k)] 'tabbar-forward)
+(define-prefix-command 'lwindow-map)
+(global-set-key (kbd "<M-up>") 'tabbar-backward-group)
+(global-set-key (kbd "<M-down>") 'tabbar-forward-group)
+(global-set-key (kbd "<M-left>") 'tabbar-backward)
+(global-set-key (kbd "<M-right>") 'tabbar-forward)
+;;;; 设置tabbar外观
+;; 设置默认主题: 字体, 背景和前景颜色，大小
+(set-face-attribute 'tabbar-default nil
+					:family "Vera Sans YuanTi Mono"
+					:background "gray80"
+					:foreground "gray30"
+					:height 1.0
+					)
+;; 设置左边按钮外观：外框框边大小和颜色
+(set-face-attribute 'tabbar-button nil
+					:inherit 'tabbar-default
+					:box '(:line-width 1 :color "gray30")
+					)
+;; 设置当前tab外观：颜色，字体，外框大小和颜色
+(set-face-attribute 'tabbar-selected nil
+					:inherit 'tabbar-default
+					:foreground "DarkGreen"
+					:background "LightGoldenrod"
+					:box '(:line-width 2 :color "DarkGoldenrod")
+					;; :overline "black"
+					;; :underline "black"
+					:weight 'bold
+					)
+;; 设置非当前tab外观：外框大小和颜色
+(set-face-attribute 'tabbar-unselected nil
+					:inherit 'tabbar-default
+					:box '(:line-width 2 :color "gray70")
+					)
+;;;################### /tabbar mode ###################
 
 (require 'redo)
 ;;设置F10为撤销
